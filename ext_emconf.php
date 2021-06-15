29eac02bbfe3db8b1babc9f6859d10832bf0c949
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2021-06-15 13:28:37 +0200

    initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Event Notifications',
+    'description' => '',
+    'category' => 'be',
+    'author' => 'Thomas Rawiel',
+    'author_email' => 'thomas.rawiel@gmail.com',
+    'state' => 'experimental',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-0.0.0',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
